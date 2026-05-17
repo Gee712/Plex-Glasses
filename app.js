991d3c75e4a94fc2f65a4f467f2700a17e45e454
Gee712 <yvtk547frs@privaterelay.appleid.com>
2026-05-17 16:42:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ================== YOUR PLEX SETTINGS ==================
-const DEFAULT_SERVER_URL = "http://23.119.50.243:32400";
+const DEFAULT_SERVER_URL = "https://23.119.50.243:32400";
 const DEFAULT_PLEX_TOKEN = "W2SAvvUNygsyESTDtgY_";
 // =======================================================
 
